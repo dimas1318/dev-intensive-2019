80f6b91bd7dc51e5895850762e6c5d0c8e78e395
dimas1318 <parshin@phystech.edu>
2020-04-05 23:00:35 +0300

    added home task 3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -7,16 +7,12 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import kotlin.math.roundToLong
 
-fun Activity.hideKeyboard(){
-    val focus = this.currentFocus
-    focus?.let {
-        (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
-            it.hideSoftInputFromWindow(focus.windowToken, 0)
-        }
-    }
+fun Activity.hideKeyboard() {
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 }
 
-fun Activity.isKeyboardOpen(): Boolean{
+fun Activity.isKeyboardOpen(): Boolean {
     val rootView = findViewById<View>(android.R.id.content)
     val visibleBounds = Rect()
     rootView.getWindowVisibleDisplayFrame(visibleBounds)
