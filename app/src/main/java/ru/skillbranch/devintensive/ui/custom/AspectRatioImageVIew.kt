512bee392a07cce3bb5c474a8d08e91282e01965
dimas1318 <parshin@phystech.edu>
2020-04-07 03:12:48 +0300

    added home task 4

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageVIew.kt
@@ -0,0 +1,33 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import ru.skillbranch.devintensive.R
+
+class AspectRatioImageVIew @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+    companion object {
+        private const val DEFAULT_ASPECT_RATION = 1.78F
+    }
+
+    private var aspectRatio = DEFAULT_ASPECT_RATION
+
+    init {
+        if (attrs != null) {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.AspectRatioImageVIew)
+            aspectRatio =
+                a.getFloat(R.styleable.AspectRatioImageVIew_aspectRatio, DEFAULT_ASPECT_RATION)
+            a.recycle()
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val newHeight = (measuredWidth / aspectRatio).toInt()
+        setMeasuredDimension(measuredWidth, newHeight)
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
@@ -0,0 +1,19 @@
+package ru.skillbranch.devintensive.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
+
+class ProfileViewModel : ViewModel() {
+    private val repository: PreferencesRepository = PreferencesRepository
+    private val profileData = MutableLiveData<Profile>()
+
+    fun getProfileData(): LiveData<Profile> = profileData
+
+    fun saveProfileData(profile: Profile) {
+        repository.saveProfile(profile)
+        profileData.value = profile
+    }
+}
